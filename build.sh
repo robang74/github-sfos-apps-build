b7be7381ac8c5684cae49e69f6bf813e18e06502
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-07-19 11:32:32 +0200

    build.sh: last is 4.5.0.16 and pull from robang74 docker hub
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- build.sh
+++ build.sh
@@ -2,12 +2,22 @@
 
 set -ex
 
-echo "Version 1"
+echo "Version 1.1"
+
+if [ "$INPUT_RELEASE" = "last" ]; then
+  INPUT_RELEASE="4.5.0.16"
+fi
+
+if [ "$INPUT_RELEASE" = "4.5.0.16" ]; then
+  provider="robang74"
+else
+  provider="coderus"
+fi
 
 docker run --rm --privileged \
-  -v $GITHUB_WORKSPACE:/workspace \
-  -v $GITHUB_ACTION_PATH:/action \
-  -e INPUT_RELEASE \
-  -e INPUT_ARCH \
-  coderus/sailfishos-platform-sdk:$INPUT_RELEASE \
-  "/bin/bash" "/action/docker.sh"
+    -v $GITHUB_WORKSPACE:/workspace \
+    -v $GITHUB_ACTION_PATH:/action \
+    -e INPUT_RELEASE \
+    -e INPUT_ARCH \
+    $provider/sailfishos-platform-sdk:$INPUT_RELEASE \
+    "/bin/bash" "/action/docker.sh"
