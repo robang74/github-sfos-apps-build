4ac857c3a5813788381c4c3870470823ee5e916e
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-07-23 19:58:22 +0200

    docker.sh: add mb2 build-init and build-requires before prepares
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- docker.sh
+++ docker.sh
@@ -5,6 +5,8 @@ set -mex
 mkdir -p build
 cd build
 cp -r /workspace/* .
+mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-init
+mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-requires
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} prepare
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} apply
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build
