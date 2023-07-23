65f535d1be2b5fe9d71fde135a1baf4e027048db
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-07-23 21:13:58 +0200

    docker.sh: GPLv2 re-licensing, copyleft header added
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- docker.sh
+++ docker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+#
+# (C) 2020 and 2022, CODeRUS <coderusinbox@gmail.com>
+# (C) 2021, Oskar Roesler (bionade24) <o.roesler@oscloud.info>
+# (C) 2022, Andrea Scarpino <andrea@scarpino.dev>
+#
+# (C) 2023, Roberto A. Foglietta <roberto.foglietta@gmail.com>
+#           Released and re-licensed under the GPLv2 license terms.
+#
+################################################################################
 
 set -mex
 
@@ -6,15 +15,15 @@ mkdir -p build
 cd build
 ret=0
 cp -r /workspace/* .
-mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-init
-mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-requires
-mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} prepare
-if ! mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} apply; then 
-  #RAF: sometimes fails but could be fine
-  echo "WARNING: some patches did not apply, check the log."
-  ret=1
-fi
+mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-init             # RAF
+mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-requires         # RAF
+mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} prepare                # RAF
+if ! mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} apply; then       # RAF
+  #RAF: sometimes fails but could be fine                               # RAF
+  echo "WARNING: some patches did not apply, check the log."            # RAF
+  ret=1                                                                 # RAF
+fi                                                                      # RAF
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build
 sudo mkdir -p /workspace/RPMS
 sudo cp -r RPMS/*.rpm /workspace/RPMS
-exit $ret
+exit $ret                                                               # RAF
