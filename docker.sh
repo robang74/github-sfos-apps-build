a5c108582624ee879e82106ec3c29de4d97dee75
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-07-23 20:39:34 +0200

    docker.sh: when apply the patches fails exit with error
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- docker.sh
+++ docker.sh
@@ -4,16 +4,17 @@ set -mex
 
 mkdir -p build
 cd build
+ret=0
 cp -r /workspace/* .
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-init
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-requires
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} prepare
-set +e
 if ! mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} apply; then 
   #RAF: sometimes fails but could be fine
   echo "WARNING: some patches did not apply, check the log."
+  ret=1
 fi
-set -e
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build
 sudo mkdir -p /workspace/RPMS
 sudo cp -r RPMS/*.rpm /workspace/RPMS
+exit $ret
