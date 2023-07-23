248a60cfd35f0ac0f28c0fd5ba98d32814bdcd27
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-07-23 21:57:41 +0200

    docker.sh: if patches fail to apply then exit error immediately
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- docker.sh
+++ docker.sh
@@ -19,11 +19,16 @@ mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-init             # RAF
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-requires         # RAF
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} prepare                # RAF
 if ! mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} apply; then       # RAF
-  #RAF: sometimes fails but could be fine                               # RAF
-  echo "WARNING: some patches did not apply, check the log."            # RAF
-  ret=1                                                                 # RAF
+  # RAF: we need to put a flag here to ignore the error but until       # RAF
+  #      such feature will be deployed, it is better to exit here       # RAF
+  #      and save the build time for the next round.                    # RAF
+  echo "========================================================"       # RAF 
+  echo                                                                  # RAF
+  echo "    ERROR: some patches did not apply, check the log.   "       # RAF
+  echo                                                                  # RAF
+  echo "========================================================"       # RAF 
+  exit 1                                                                # RAF
 fi                                                                      # RAF
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build
 sudo mkdir -p /workspace/RPMS
 sudo cp -r RPMS/*.rpm /workspace/RPMS
-exit $ret                                                               # RAF
