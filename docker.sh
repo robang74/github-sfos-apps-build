46eb4dfb8799f59453be0d2d33689b669001659a
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-07-23 20:17:56 +0200

    docker.sh: paches apply can fail but the build can continue
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- docker.sh
+++ docker.sh
@@ -8,7 +8,12 @@ cp -r /workspace/* .
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-init
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build-requires
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} prepare
-mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} apply
+set +e
+if ! mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} apply; then 
+  #RAF: sometimes fails but could be fine
+  echo "WARNING: some patches did not apply, check the log."
+fi
+set -e
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build
 sudo mkdir -p /workspace/RPMS
 sudo cp -r RPMS/*.rpm /workspace/RPMS
