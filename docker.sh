f920da243e12c5bd670be7ec886f005c62ad5211
Roberto A. Foglietta <roberto.foglietta@gmail.com>
2023-07-23 19:47:53 +0200

    docker.sh: add mb2 prepare and apply for patches
    Signed-off-by: Roberto A. Foglietta <roberto.foglietta@gmail.com>

--- docker.sh
+++ docker.sh
@@ -5,6 +5,8 @@ set -mex
 mkdir -p build
 cd build
 cp -r /workspace/* .
+mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} prepare
+mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} apply
 mb2 -t SailfishOS-${INPUT_RELEASE}-${INPUT_ARCH} build
 sudo mkdir -p /workspace/RPMS
 sudo cp -r RPMS/*.rpm /workspace/RPMS
